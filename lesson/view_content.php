d9f85dca18435d9e9e1f239290d9b44974caf0b9
Fyr <fyr.work@gmail.com>
2013-09-29 20:34:53 +0200

    Перенес модули в др.папки и отрефакторил код для view

--- lesson/view_content.php
+++
@@ -1,5 +0,0 @@
-<?
-	// $sampleContent = '<div class="container-scroll-box"><div class="container-scroll-box-holder"> <h2>Заголовок...</h2><p>Здесь должен быть какой-то текст...</p> </div></div>';
-	// $contentHTML = ($contentHTML) ? $contentHTML : $sampleContent;
-?>
-			<div class="container-scroll" id="mercury-content" data-mercury="full"><?=$contentHTML?></div>
