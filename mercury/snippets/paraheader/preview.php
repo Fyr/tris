a9952eb9b18180d273404830816688589c5eaa69
Fyr <fyr.work@gmail.com>
2013-09-29 20:43:08 +0200

    Перенес модули в др.папки и отрефакторил код для view

--- mercury/snippets/paraheader/preview.php
+++
@@ -1,9 +0,0 @@
-<?
-	$edit = isset($_POST['edit']) && $_POST['edit'];
-	$style = (isset($_POST['margin_left']) && $_POST['margin_left']) ? ' margin-left: '.$_POST['margin_left'].'%;' : '';
-	$style.= (isset($_POST['margin_right']) && $_POST['margin_right']) ? ' margin-right: '.$_POST['margin_right'].'%;' : '';
-?>
-<style type="text/css">
-@import "/mercury/snippets/determination/css/style.css";
-</style>
-<div align="<?=@$_POST['align']?>" style="<?=$style?>"> <h2> <span <? if ($edit) {?>contenteditable="true" onchange="onChangeEditable(this, 'editable')"<? } ?>><?=@$_POST['editable']?></span></h2></div>
